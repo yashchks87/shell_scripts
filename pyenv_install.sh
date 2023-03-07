cfb7c7871f70cbdf72d94b6980268ccb2c4c2ecc
yashchks87 <yashchks87@gmail.com>
2023-03-07 23:34:49 +0000

    Pyenv install issues fixed.

--- pyenv_install.sh
+++ pyenv_install.sh
@@ -1,3 +1,6 @@
+# For this script please follow this link
+# Link: https://www.liquidweb.com/kb/how-to-install-pyenv-on-ubuntu-18-04/
+
 sudo apt update -y
 
 sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
