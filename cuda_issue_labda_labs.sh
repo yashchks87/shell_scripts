b275c644a6647c5716354f5b1385618b833d9faf
yashchks87 <yashchks87@gmail.com>
2023-03-26 19:20:50 +0000

    New cuda lambda labs fixed.

--- cuda_issue_labda_labs.sh
+++ cuda_issue_labda_labs.sh
@@ -5,6 +5,8 @@ sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/
 sudo apt-get update
 # Main version of cuda
 sudo apt-get install libcudnn8
+# Please use this for most of tensorflow after 2.9 verisons
+# sudo apt-get install libcudnn8=8.6.0.163-1+cuda11.8
 # Only use if you want to use cuda with developer versions only.
 # Please check this link if tensorflow gets cuda error: 
 # Link: https://www.tensorflow.org/install/source#gpu
